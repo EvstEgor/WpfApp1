e7e770ac01ab31b44f7cfe960ab5ddfdcdc57929
(EvstEgor) <(egorevst4567@gmail.com)>
2025-04-08 10:58:11 +0300

    Revert "исправил ошибки"
    This reverts commit 72a1c5ea12b76f1afe9d450e2dc1e5d4de22d7fe.

--- WpfApp1/Hotel.cs
+++ WpfApp1/Hotel.cs
@@ -1,18 +1,18 @@
-﻿    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-    namespace WpfApp1
+namespace WpfApp1
+{
+    public class Hotel
     {
-        public class Hotel
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public int CountOfStaits { get; set; }
-            public string CountryName { get; set; }
-            public string HotelImage { get; set; }
-        }
-
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stars { get; set; }
+        public string Location { get; set; }
+        public string ImageUrl { get; set; }
     }
+
+}
